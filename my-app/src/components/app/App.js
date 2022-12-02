c98ef4b51290189689ca574b88bfde76dbca9588
Tatiana <tmoseeva@hotmail.com>
2022-12-02 13:58:33 +1100

    add a data array and create a new component for a list item and import item to the list

--- my-app/src/components/app/App.js
+++ my-app/src/components/app/App.js
@@ -1,14 +1,28 @@
-import React from 'react';
+import {useState} from 'react';
+import { nanoid } from 'nanoid'
 
 import Header from '../header/Header';
 import List from '../list/List';
 import AddNew from '../add-new/AddNew';
 
+function id() {
+	return nanoid();
+}
+
+const initData = [
+    {id: id(), desc: 'Make a mess in the kitchen by creating snow angels in flour', date: '2 days ago', done: false},
+    {id: id(), desc: 'Rearrange all the furniture overnight', date: '5 days ago', done: false},
+    {id: id(), desc: 'Park the car in the middle of the garden', date: '3 days ago', done: false},
+    {id: id(), desc: 'Have a marshmallow snowball fight with other soft toys', date: '3 days ago', done: false},
+]
 
 function App() {
+
+    const [tasks, setTasks] = useState(initData);
+
 	return <div className='container'>
 		<Header />
-        <List />
+        <List tasks={tasks}/>
         <AddNew />
 	</div>;
 }
--- my-app/src/components/list/List.js
+++ my-app/src/components/list/List.js
@@ -1,46 +1,24 @@
+import ListItem from '../list-item/ListItem';
+
 import "./List.css";
 
 
-function List() {
+function List({tasks}) {
+
+    const items = tasks.map(task => {
+        return  <ListItem
+                    key = {task.id}
+                    desc = {task.desc}
+                    date = {task.date}
+                    done = {task.done}
+                />
+    })
+
 	return <div className="list">
-
-        <ul className="list-group">
-
-            <li  className="list-group-item list-group-item-action " aria-current="true">
-            <div className="d-flex w-100 justify-content-between">
-                <h5 className="mb-1">List group item heading List group item heading</h5>
-                <small>3 days ago</small>
-            </div>
-            <div className="d-flex w-100 justify-content-end">
-                <i className="fas fa-pen"></i>
-                <i className="fas fa-trash"></i>
-            </div>
-            </li>
-
-            <li  className="list-group-item list-group-item-action" aria-current="true">
-            <div className="d-flex w-100 justify-content-between">
-                <h5 className="mb-1 done">List group item heading</h5>
-                <small>3 days ago</small>
-            </div>
-            <div className="d-flex w-100 justify-content-end">
-                <i className="fas fa-pen"></i>
-                <i className="fas fa-trash"></i>
-            </div>
-            </li>
-
-            <li className="list-group-item list-group-item-action " aria-current="true">
-            <div className="d-flex w-100 justify-content-between">
-                <h5 className="mb-1">List group item heading</h5>
-                <small>3 days ago</small>
-            </div>
-            <div className="d-flex w-100 justify-content-end">
-                <i className="fas fa-pen"></i>
-                <i className="fas fa-trash"></i>
-            </div>
-            </li>
-
-        </ul>
-    </div>
+                <ul className="list-group">
+                    {items}
+                </ul>
+        </div>
 }
 
 export default List;
